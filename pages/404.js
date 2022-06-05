75018c3a393afed545cffcd74de9ff55dabea6e5
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-06-05 19:14:11 +0300

    Added locale detection in 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+
 import styles from "@/styles/404.module.css";
 
 const NotFound = () => {
+	const router = useRouter();
+	const text = useRef();
+
+	useEffect(() => {
+		if (router.asPath.substring(1, 3) === "en") {
+			text.current.textContent =
+				"Oops... Seems like the page you are looking for doesn't exist";
+		} else {
+			text.current.textContent = "Η σελίδα που ψάχνεις δεν υπάρχει...";
+		}
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<h1>404</h1>
-			<h2>
-				Oops... Seems like the page you are looking for doesn&apos;t exist
-			</h2>
+			<h2 ref={text}></h2>
 		</div>
 	);
 };
