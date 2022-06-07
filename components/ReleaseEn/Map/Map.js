8f6fe50dcfc442b5cfc0094b935165d19be55caa
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-06-07 12:27:16 +0300

    Added static font files. Updated Map component with correct map preview image and link.

--- components/ReleaseEn/Map/Map.js
+++ components/ReleaseEn/Map/Map.js
@@ -5,9 +5,9 @@ import styles from "@/components/Release/Map/Map.module.css";
 const Map = () => {
 	return (
 		<section className={styles.section} id='location-map'>
-			<Link href='https://www.google.com/maps/d/u/0/embed?mid=14z_afsp6RehbaoqFzvjweyBgQkHixOXV&ehbc=2E312F'>
+			<Link href='https://www.google.com/maps/d/u/5/edit?mid=15M3d_kYuAwvhAp34OnV9G0mgD2r9H4Y&usp=sharing'>
 				<a target='_blank' rel='noopener,noreferrer'>
-					<img className={styles.map} src='/map.png' alt='Location Map' />
+					<img className={styles.map} src='/map/map.png' alt='Location Map' />
 					<p>Click to see the map</p>
 				</a>
 			</Link>
